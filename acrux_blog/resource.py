b555e86c49229532f6712d79c7958e93d5acd4e3
Douglas Bastos <douglashsb@gmail.com>
2015-08-17 20:43:14 -0300

    Cria arquivo de conexão ao redis

---
+++ acrux_blog/resource.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+import redis
+import json
+from django.conf import settings
+
+
+class Resource(object):
+
+    def __init__(self):
+        self._config()
+        self._client = self.connect()
+
+    def _config(self):
+        self._host = settings.ACRUX_BLOG['host']
+        self._port = settings.ACRUX_BLOG['port']
+        self._pass = settings.ACRUX_BLOG['pass']
+        self._db = settings.ACRUX_BLOG['db']
+
+    def connect(self):
+        return redis.StrictRedis(host=self._host, port=self._port, db=self._db,
+                                 password=self._pass)
+
+    def get(self, key):
+        result = self._client.get(key)
+        if result:
+            try:
+                result = json.loads(result)
+            except ValueError:
+                pass
+        return result
+
+    def set(self, key, value):
+        try:
+            value = json.dumps(value)
+        except ValueError:
+            pass
+        return self._client.set(key, value)
+
+    def zadd(self, key, **kwargs):
+        return self._client.zadd(key, **kwargs)
+
+    def zrange(self, key, reverse=True):
+        result = self._client.zrange(key, 0, -1, withscores=False)
+        if reverse:
+            return reversed(result)
+        return result
