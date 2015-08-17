321afadbec1f5c6d2bfd67c43673d9b13a671203
Douglas Bastos <douglashsb@gmail.com>
2015-08-17 20:20:37 -0300

    Muda rota de urls

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls import url
 from .views import blog, blog_redis
 
 urlpatterns = [
-    # url(r'base', blog.base, name='base'),
-    url(r'index.html', blog.ListPosts.as_view(), name='page_index'),
+    # Banco relacional
+    url(r'blog/', blog.ListPosts.as_view(), name='page_index'),
     url(r'blog/(?P<slug>[a-z0-9\-]+)/$', blog.DetailPost.as_view(), name='post'),
 
-    #redis
-    url(r'redis.html', blog_redis.ListPosts.as_view(), name='page_index_with_redis'),
+    # redis
+    url(r'redis/', blog_redis.ListPosts.as_view(), name='page_index_with_redis'),
     url(r'redis/(?P<slug>[a-z0-9\-]+)/$', blog_redis.DetailPost.as_view(), name='posts_with_redis'),
 ]   
