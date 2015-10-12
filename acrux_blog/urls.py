c8ab414f7a5228c9407110d12e7fc50ce834b1ea
Douglas Bastos <douglashsb@gmail.com>
2015-10-12 11:49:46 -0300

    Adiciona cache

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from .views import blog
 
 urlpatterns = [
     # url(r'base', blog.base, name='base'),
-    url(r'index.html$', blog.ListPosts.as_view(), name='page_index'),
-    url(r'(?P<slug>[a-z0-9\-]+)/$', blog.DetailPost.as_view(), name='post'),
+    url(r'index.html$', cache_page(60 * 15)(blog.ListPosts.as_view()), name='page_index'),
+    url(r'(?P<slug>[a-z0-9\-]+)/$', cache_page(60 * 15)(blog.DetailPost.as_view()), name='post'),
 ]   
