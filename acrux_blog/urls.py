9656e2be67830dbfc8f9410dc76e669a00ef349a
Douglas Bastos <douglashsb@gmail.com>
2015-10-19 14:31:52 -0200

    defini urls do blog

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -5,7 +5,8 @@ from django.views.decorators.cache import cache_page
 from .views import blog
 
 urlpatterns = [
-    # url(r'base', blog.base, name='base'),
-    url(r'index.html$', cache_page(60 * 15)(blog.ListPosts.as_view()), name='page_index'),
-    url(r'(?P<slug>[a-z0-9\-]+)/$', cache_page(60 * 15)(blog.DetailPost.as_view()), name='post'),
-]   
+    url(r'^index.html$', (blog.ListPosts.as_view()), name='page_index'),
+    url(r'^post/author/(?P<author>[a-z0-9\-]+)/$', (blog.ListPostsAuthor.as_view()), name='author_posts'),
+    url(r'^post/tag/(?P<tag>[a-z0-9\-]+)$', (blog.ListPostsTag.as_view()), name='tags_posts'),
+    url(r'^post/slug/(?P<slug>[a-z0-9\-]+)/$', (blog.DetailPost.as_view()), name='post'),
+]
