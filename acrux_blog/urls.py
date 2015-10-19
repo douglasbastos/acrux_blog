454da93bbd6561c0a7eb413e3e41f9e2979b51ec
Douglas Bastos <douglashsb@gmail.com>
2015-10-19 19:14:57 -0200

    Home fica no /

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 from .views import blog
 
 urlpatterns = [
-    url(r'^index.html$', (blog.ListPosts.as_view()), name='page_index'),
+    url(r'^$', (blog.ListPosts.as_view()), name='page_index'),
     url(r'^post/author/(?P<author>[a-z0-9\-]+)/$', (blog.ListPostsAuthor.as_view()), name='author_posts'),
     url(r'^post/tag/(?P<tag>[a-z0-9\-]+)$', (blog.ListPostsTag.as_view()), name='tags_posts'),
     url(r'^post/slug/(?P<slug>[a-z0-9\-]+)/$', (blog.DetailPost.as_view()), name='post'),
