41f3c4eeab346d4587d77401a2c8052ac2d3e2d7
Douglas Bastos <douglashsb@gmail.com>
2015-11-02 15:38:31 -0200

    Permite nome do author com letras maiúscula na url

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', (views.ListPosts.as_view()), name='page_index'),
-    url(r'^post/author/(?P<author>[a-z0-9\-]+)/$', (views.ListPostsAuthor.as_view()), name='author_posts'),
+    url(r'^post/author/(?P<author>[a-zA-Z0-9\-]+)/$', (views.ListPostsAuthor.as_view()), name='author_posts'),
     url(r'^post/tag/(?P<tag>[a-z0-9\-]+)$', (views.ListPostsTag.as_view()), name='tags_posts'),
     url(r'^post/slug/(?P<slug>[a-z0-9\-]+)/$', (views.DetailPost.as_view()), name='post'),
 ]
