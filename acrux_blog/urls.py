08b7fa1551a60e238897e6b6b143e533d6b9ee36
Douglas Bastos <douglashsb@gmail.com>
2015-11-14 02:07:05 -0200

    Alter name urls

--- acrux_blog/urls.py
+++ acrux_blog/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     url(r'^post/slug/(?P<slug>[a-z0-9\-]+)/$', views.DetailPost.as_view(), name='post'),
 
     # with cache
-    url(r'^cache/', cache_page(settings.TIME_CACHE)(views.ListPosts.as_view()), name='page_index'),
-    url(r'^post_cache/author/(?P<author>[a-zA-Z0-9\-]+)/$', cache_page(settings.TIME_CACHE)(views.ListPostsAuthor.as_view()), name='author_posts'),
-    url(r'^post_cache/tag/(?P<tag>[a-z0-9\-]+)$', cache_page(settings.TIME_CACHE)(views.ListPostsTag.as_view()), name='tags_posts'),
-    url(r'^post_cache/slug/(?P<slug>[a-z0-9\-]+)/$', cache_page(settings.TIME_CACHE)(views.DetailPost.as_view()), name='post'),
+    url(r'^cache/', cache_page(settings.TIME_CACHE)(views.ListPosts.as_view()), name='page_index_cache'),
+    url(r'^post_cache/author/(?P<author>[a-zA-Z0-9\-]+)/$', cache_page(settings.TIME_CACHE)(views.ListPostsAuthor.as_view()), name='author_posts_cache'),
+    url(r'^post_cache/tag/(?P<tag>[a-z0-9\-]+)$', cache_page(settings.TIME_CACHE)(views.ListPostsTag.as_view()), name='tags_posts_cache'),
+    url(r'^post_cache/slug/(?P<slug>[a-z0-9\-]+)/$', cache_page(settings.TIME_CACHE)(views.DetailPost.as_view()), name='post_cache'),
 ]
